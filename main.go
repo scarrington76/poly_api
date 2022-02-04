be8506b4b013f50e2974175b3510be9a0321a943
Scott Carrington <scarrington@gmail.com>
2022-02-03 20:31:08 -0700

    test handler

--- main.go
+++ main.go
@@ -1,23 +1,63 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
+// CORS Middleware
+func CORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// Set headers
+		w.Header().Set("Access-Control-Allow-Headers:", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		// Next
+		next.ServeHTTP(w, r)
+		return
+	})
+}
+
+type timeHandler struct {
+	format string
+}
+
+func GETHandler(w http.ResponseWriter, r *http.Request) {
+
+	th := timeHandler{format: time.RFC1123}
+	tm := time.Now().Format(th.format)
+	w.Write([]byte("The time is: " + tm))
+}
+
 func main() {
-	r := gin.Default()
+	r := mux.NewRouter()
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 
-	r.GET("/hello", func(c *gin.Context) {
-		c.String(200, "Hello, World!")
-	})
+	// We use our custom CORS Middleware
+	r.Use(CORS)
 
-	api := r.Group("/api")
+	fmt.Print("API initiated")
+	r.HandleFunc("/tests", GETHandler)
+	// r.HandleFunc("/api/insert", POSTHandler)
+	// r.HandleFunc("/api/portfolio", GetPortfolio)
+	// r.HandleFunc("/api/account", GETAccountValue)
+	// r.HandleFunc("/api/indexes", GetIndexes)
 
-	api.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-
-	r.Run()
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
